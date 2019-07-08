d039ed1e3a214abc0a9bbaff49a4ba69a264671f
Yoon Jihwan <youwlghks@gmail.com>
2019-07-08 15:38:01 +0900

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-    $host = "localhost";
-    $dbid = "youwlghks";                    // 자신의 DB ID
-    $dbpass = "wlghks0319";                  // 자신의 DB Password
-    $dbname = "youwlghks";                  // 해당 프로젝트에서 사용하는 DB명
-?>
